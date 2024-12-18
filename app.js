1d9fc0cdcd54b4f63fd08b165dfd547e70828cb4
Tolgahan Gümüş <adalix@Tolgahans-MacBook-Pro.local>
2024-12-19 01:19:23 +0300

    changed something

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
-
-const button = document.querySelector('button')
-console.log(button)
-
 canvas.width = innerWidth
 canvas.height = innerHeight
 
